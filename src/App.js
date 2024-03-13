4fcccb863a8557b9a3534af5b52b1b469deeb9e1
Kunalsinh <kunal4as.it@gmail.com>
2024-03-14 03:04:45 +0530

    Suggestions Component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Suggestion from './Suggestions';
+
+function App() {
+  return (
+    <div className="App">
+      <Suggestion />
+    </div>
+  );
+}
+
+export default App;

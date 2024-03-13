ad498c1e88f9f9a60161949e4c6ae7f96537768d
Kunalsinh <kunal4as.it@gmail.com>
2024-03-14 03:38:48 +0530

    Changed data from static to dynamic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Suggestion from './Suggestions';
+import logo from "./logo.svg";
+import "./App.css";
+import Suggestion from "./Suggestions";
+const allData = require("./data.json");
 
 function App() {
+  console.log(allData);
   return (
-    <div className="App">
-      <Suggestion />
-    </div>
+    <>
+      {
+      allData.map(function (suggest) {
+        return <Suggestion suggestionPic={suggest.suggestPic} suggestionName={suggest.suggestName} />
+        
+      })
+      }
+    </>
   );
 }
 
--- src/Suggestions.js
+++ src/Suggestions.js
@@ -1,20 +1,18 @@
-function Suggestion() {
+function Suggestion(props) {
     return (
         <>
             <div style={{ height: "50px", width: "300px", border:"0px" , padding:"0px", marginLeft:"50px"}}>
                 <img
-                    src="assets/group.jpeg"
+                    src={props.suggestionPic}
                     alt="image"
                     height="50px" 
                     width="50px"
                     style={{ float: "left", border:"0px", borderRadius:"16px"}}
                 />
-
-                {/* <div> */}
-                    <p style={{fontSize:"20px",paddingTop:"9px",fontWeight:"normal",paddingRight:"50px"}}>
-                        Fashion Designer
+                    <p style={{fontSize:"20px",paddingTop:"9px",fontWeight:"normal",marginLeft:"80px"}}>
+                        {props.suggestionName}
                     </p>
-                {/* </div> */}
+                
             </div>
         </>
     );
